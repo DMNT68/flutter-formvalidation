a1931b557b4302abb4cebaf743e2bb405791b04f
Andrés Salgado <andressalgado41@gmail.com>
2020-10-07 08:56:53 -0500

    productos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+
 import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+    
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final productosProvider = new ProductosProvider();
+
   @override
   Widget build(BuildContext context) {
 
@@ -12,15 +23,60 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Email: ${bloc.email}'),
-          Divider(),
-          Text('Password: ${bloc.password}')
-        ],
+      body: _crearListado(),
+      floatingActionButton: _crearBoton(context),
+    );
+  }
+
+  Widget _crearListado() {
+
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder: (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+        if ( snapshot.hasData ) {
+          
+          final productos = snapshot.data;
+          return ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (context,i) => _crearItem(context, productos[i])
+          );
+
+        } else {
+          
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+
+        }
+      },
+    );
+
+  }
+
+  Widget _crearItem(BuildContext context ,ProductoModel producto) {
+
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red
       ),
+      onDismissed: (direccion) {
+        productosProvider.borrarProducto(producto.id);
+      },
+      child: ListTile(
+        title: Text('${producto.titulo} - ${producto.valor}'),
+        subtitle: Text(producto.id),
+        onTap: () => Navigator.pushNamed(context, 'producto', arguments: producto).then((value) { setState(() { }); }),
+      ),
+    );
+
+  }
+
+  Widget _crearBoton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () => Navigator.pushNamed(context, 'producto').then((value) { setState(() { });}),
+      backgroundColor: Colors.deepPurple,
     );
   }
 }
